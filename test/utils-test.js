cb1bc507af08c47d5e892db232010eaf5bd4faac
Hector Flores <hecto932@gmail.com>
2016-11-01 01:08:26 -0600

    Pruebas unitarias para validar un array

--- test/utils-test.js
+++ test/utils-test.js
@@ -1,17 +1,25 @@
 'use strict'
 
 const test = require('ava')
+const utils = require('../lib/utils')
 
-test('this should pass', t => {
-  t.pass()
-})
+test('extracting hashtags from text', t => {
+  let tags = utils.extractTags('a #picture with tags #AwEsOmE #Platzi #AVA and #100 ##yes')
+  t.deepEqual(tags, [
+    'picture',
+    'awesome',
+    'platzi',
+    'ava',
+    '100',
+    'yes'
+  ])
 
-test('this should fail', t => {
-  t.fail()
-})
+  tags = utils.extractTags('a picture with no tags')
+  t.deepEqual(tags, [])
 
-test('it should support async/await', async t => {
-  let p = Promise.resolve(42)
-  let secret = await p
-  t.is(secret, 42)
+  tags = utils.extractTags()
+  t.deepEqual(tags, [])
+
+  tags = utils.extractTags(null)
+  t.deepEqual(tags, [])
 })
