a0f5c250c46f4545245f553a5b5cc4971a073895
Hector Flores <hecto932@gmail.com>
2016-11-03 20:11:20 -0600

    Authentication de usuario mediante password

--- test/db-test.js
+++ test/db-test.js
@@ -106,3 +106,18 @@ test('get user', async t => {
 
   t.deepEqual(created, result)
 })
+
+test('authenticate user', async t => {
+  let db = t.context.db
+  t.is(typeof db.authenticate, 'function', 'authenticate is a function')
+
+  let user = fixtures.getUser()
+  let plainPassword = user.password
+  await db.saveUser(user)
+
+  let success = await db.authenticate(user.username, plainPassword)
+  t.true(success)
+
+  let fail = await db.authenticate(user.username, 'foo')
+  t.false(fail)
+})
