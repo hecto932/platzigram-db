3397af111f9e681963ca7132bed488a5bce36805
Hector Flores <hecto932@gmail.com>
2016-11-04 00:14:16 -0600

    Preparando nuestro modulo para microservicios

--- test/db-test.js
+++ test/db-test.js
@@ -9,7 +9,7 @@ const fixtures = require('./fixtures')
 
 test.beforeEach('setup database', async t => {
   const dbName = `platzigram_${uuid.v4()}`
-  const db = new Db({ db: dbName })
+  const db = new Db({ db: dbName, setup: true })
   await db.connect()
   t.context.db = db
   t.context.dbName = dbName
