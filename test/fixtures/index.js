fee72094325f8256d9da96029d842e2ec0acad75
Hector Flores <hecto932@gmail.com>
2016-11-27 04:22:19 -0600

    Arreglando algunos problemas

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -6,7 +6,7 @@ const fixtures = {
   getImage () {
     return {
       description: 'an #awesome picture with #tags #platzi',
-      url: `http://platzigram.test/${uuid.v4()}.jpg`,
+      url: `https://platzigram.test/${uuid.v4()}.jpg`,
       likes: 0,
       liked: false,
       userId: uuid.uuid()
@@ -17,14 +17,15 @@ const fixtures = {
     while (n-- > 0) {
       images.push(this.getImage())
     }
+
     return images
   },
   getUser () {
     return {
       name: 'A random user',
-      username: `user_${uuid.uuid()}`,
+      username: `user_${uuid.v4()}`,
       password: uuid.uuid(),
-      email: `${uuid.v4()}@domain.test`
+      email: `${uuid.v4()}@platzi.test`
     }
   }
 }
